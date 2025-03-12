7b74556b231c8c256360e07a602684a1588488c2
Joel B. <smol@toast.cyou>
2025-03-12 01:43:40 -0400

    new icon draft

--- flake.nix
+++ flake.nix
@@ -64,6 +64,7 @@
         postBuild = ''
             # for desktop files
             install -Dt $out/share/applications resources/admiral.desktop
+            install -Dt $out/share/icons resources/admiral.png
         '';
       };
     in {
