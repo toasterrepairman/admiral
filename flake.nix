7bf58a647ee3a447170cd21349dffd6b980625eb
Joel B. <smol@toast.cyou>
2025-11-05 15:22:06 -0500

    unified name

--- flake.nix
+++ flake.nix
@@ -92,7 +92,7 @@
 
         postBuild = ''
             # for desktop files
-            install -Dt $out/share/applications resources/com.github.admiral.desktop
+            install -Dt $out/share/applications resources/com.toasterrepair.Admiral.desktop
 
             install -Dt $out/share/icons resources/icon-admiral.png
         '';
